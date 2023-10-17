fdd1c7fc41b0ec13fd87b7e677ab92a62f35284e
dlvin <dlvinhafeez84@gmail.com>
2023-10-16 18:00:12 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import WeatherApp from './components/weatherApp/WeatherApp';
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
